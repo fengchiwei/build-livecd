819987bb495280b1889b89ecff5905d61adb6c47
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2006-08-31 21:08:05 +0000

    Import build-livecd-r166 into Google Code

---
+++ post-config/etc/X11/ion3/cfg_menus.lua
@@ -0,0 +1,87 @@
+--
+-- Ion menu definitions
+--
+
+
+-- Main menu
+defmenu("mainmenu", {
+    submenu("執行程式",         "appmenu"),
+--    menuentry("Lock screen",
+--            "ioncore.exec_on(_, ioncore.lookup_script('ion-lock'))"),
+--    menuentry("Help",           "mod_query.query_man(_)"),
+    submenu("佈景主題",           "stylemenu"),
+--	submenu("Debian",           "Debian"),
+    submenu("X Window 程序",          "sessionmenu"),
+})
+
+
+-- Application menu
+defmenu("appmenu", {
+    menuentry("終端機 (rxvt-unicode)",       "ioncore.exec_on(_, '/usr/bin/rxvt.sh')"), --x-terminal-emulator
+    menuentry("瀏覽器 (firefox)",        "ioncore.exec_on(_, 'firefox')"), -- sensible-browser')"),
+    submenu("網路應用",         "netmenu"),
+    submenu("多媒體",         "mediamenu"),
+    submenu("系統工具",         "adminmenu"),
+    menuentry("執行...",         "mod_query.query_exec(_)"),
+})
+
+-- Net application menu
+defmenu("netmenu", {
+    menuentry("網頁瀏覽 (firefox)",          "ioncore.exec_on(_, 'firefox')"), 
+    menuentry("即時通訊 (gaim)",          "ioncore.exec_on(_, 'gaim')"), 
+    menuentry("BBS 連線 (pcmanx)",          "ioncore.exec_on(_, 'pcmanx')"),
+    menuentry("P2P 下載 (amule)",          "ioncore.exec_on(_, 'amule')"),
+    menuentry("FTP 連線 (gftp)",          "ioncore.exec_on(_, 'gftp')"),
+    menuentry("無線網卡驅動 (ndisgtk)", "ioncore.exec_on(_, 'sudo ndisgtk')"),
+})
+
+-- Multimedia application menu
+defmenu("mediamenu", {
+    menuentry("音樂播放 (beep-media-player)",          "ioncore.exec_on(_, 'beep-media-player')"), 
+    menuentry("影音播放 (gxine)",          "ioncore.exec_on(_, 'gxine')"), 
+    menuentry("圖片瀏覽 (gqview)",          "ioncore.exec_on(_, 'gqview')"),
+})
+
+-- Admin application menu
+defmenu("adminmenu", {
+    menuentry("PDF 閱讀 (epdfview)",          "ioncore.exec_on(_, 'epdfview')"), 
+    menuentry("CHM 閱讀 (xchm)",          "ioncore.exec_on(_, 'xchm')"), 
+    menuentry("文字編輯 (gvim)",          "ioncore.exec_on(_, 'gvim')"),
+    menuentry("檔案管理 (thunar)",          "ioncore.exec_on(_, 'thunar')"),
+    menuentry("解壓縮程式 (xarchiver)",          "ioncore.exec_on(_, 'xarchiver')"),
+    menuentry("記事本 (leafpad)",          "ioncore.exec_on(_, 'leafpad')"),
+})
+
+-- Session control menu
+defmenu("sessionmenu", {
+    menuentry("儲存現在狀態",           "ioncore.snapshot()"),
+    menuentry("重新啟動 Ion",        "ioncore.restart()"),
+--    menuentry("執行 TWM",    "ioncore.restart_other('twm')"),
+    menuentry("結束圖形介面",           "ioncore.shutdown()"),
+    menuentry("關於 Ion",      "mod_query.show_about_ion(_)"),
+--    menuentry("重新啟動",          "ioncore.exec_on(_, 'sudo reboot')"),
+--    menuentry("關機",          "ioncore.exec_on(_, 'sudo shotdown now')"),
+})
+
+
+-- Context menu (frame/client window actions)
+defctxmenu("WFrame", {
+    menuentry("關閉程式",          "WRegion.rqclose_propagate(_, _sub)"),
+    menuentry("強制關閉",           "WClientWin.kill(_sub)",
+                                "_sub:WClientWin"),
+    menuentry("加上標記",     "WRegion.set_tagged(_sub, 'toggle')",
+                                "_sub:non-nil"),
+    menuentry("貼上已標記視窗",  "WFrame.attach_tagged(_)"),
+    menuentry("清除標記",     "ioncore.clear_tags()"),
+    menuentry("窗框資訊",    "mod_query.show_clientwin(_, _sub)",
+                                "_sub:WClientWin"),
+})
+
+-- Auto-generated Debian menu definitions
+if os.execute("test -x /usr/bin/update-menus") == 0 then
+    if ioncore.is_i18n() then
+        dopath("debian-menu-i18n")
+    else
+        dopath("debian-menu")
+    end
+end
---
+++ post-config/etc/X11/ion3/look_penk.lua
@@ -0,0 +1,132 @@
+-- look-ios.lua drawing engine configuration file for Ion.
+
+if not gr.select_engine("de") then return end
+
+de.reset()
+
+de.defstyle("*", {
+    shadow_colour = "#606060",
+    highlight_colour = "#ffffff",
+    background_colour = "#d8d8d8",
+    foreground_colour = "#000000",
+    padding_pixels = 3,
+    highlight_pixels = 1,
+    shadow_pixels = 1,
+    border_style = "elevated",
+--    font = "-*-helvetica-medium-r-normal-*-14-*-*-*-*-*-*-*",
+    font = "-*-ar pl shanheisun uni-medium-r-normal--14-*-*-*-*-*-*-*",
+    text_align = "center",
+})
+
+de.defstyle("frame", {
+    based_on = "*",
+    shadow_colour = "#606060",
+    highlight_colour = "#ffffff",
+    padding_colour = "#d8d8d8",
+    background_colour = "#000000",
+    foreground_colour = "#000000",
+    padding_pixels = 4,
+    highlight_pixels = 1,
+    shadow_pixels = 1,
+    de.substyle("active", {
+        shadow_colour = "#606060",
+        highlight_colour = "#ffffff",
+        background_colour = "#d8d8d8",
+        foreground_colour = "#000000",
+    }),
+})
+
+de.defstyle("frame-ionframe", {
+    based_on = "frame",
+    border_style = "inlaid",
+    padding_pixels = 3,
+    spacing = 0,
+})
+
+de.defstyle("frame-floatframe", {
+    based_on = "frame",
+    border_style = "ridge"
+})
+
+de.defstyle("tab", {
+    based_on = "*",
+--    font = "-*-helvetica-bold-r-normal-*-16-*-*-*-*-*-*-*",
+    font = "-*-ar pl shanheisun uni-medium-r-normal--16-*-*-*-*-*-*-*",
+    de.substyle("active-selected", {
+        shadow_colour = "#f09000",
+        highlight_colour = "#f0f066",
+        background_colour = "#f0c000",
+        foreground_colour = "#000000",
+    }),
+    de.substyle("active-unselected", {
+        shadow_colour = "#606060",
+        highlight_colour = "#ffffff",
+        background_colour = "#d8d8d8",
+        foreground_colour = "#000000",
+    }),
+    de.substyle("inactive-selected", {
+        shadow_colour = "#606060",
+        highlight_colour = "#efefef",
+        background_colour = "#a8a8a8",
+        foreground_colour = "#000000",
+    }),
+    de.substyle("inactive-unselected", {
+        shadow_colour = "#606060",
+        highlight_colour = "#ffffff",
+        background_colour = "#d8d8d8",
+        foreground_colour = "#000000",
+    }),
+    text_align = "center",
+})
+
+de.defstyle("tab-frame", {
+    based_on = "tab",
+    de.substyle("*-*-*-*-activity", {
+        shadow_colour = "#606060",
+        highlight_colour = "#ffffff",
+        background_colour = "#990000",
+        foreground_colour = "#eeeeee",
+    }),
+})
+
+de.defstyle("tab-frame-ionframe", {
+    based_on = "tab-frame",
+    spacing = 0,
+})
+
+de.defstyle("tab-menuentry", {
+    based_on = "tab",
+    text_align = "left",
+    highlight_pixels = 0,
+    shadow_pixels = 0,
+})
+
+de.defstyle("tab-menuentry-big", {
+    based_on = "tab-menuentry",
+--    font = "-*-helvetica-medium-r-normal-*-17-*-*-*-*-*-*-*",
+    font = "-*-ar pl shanheisun uni-medium-r-normal--16-*-*-*-*-*-*-*",
+    padding_pixels = 7,
+})
+
+de.defstyle("input", {
+    based_on = "*",
+    shadow_colour = "#606060",
+    highlight_colour = "#ffffff",
+    background_colour = "#d8d8d8",
+    foreground_colour = "#000000",
+    padding_pixels = 1,
+    highlight_pixels = 1,
+    shadow_pixels = 1,
+    border_style = "elevated",
+    de.substyle("*-cursor", {
+        background_colour = "#000000",
+        foreground_colour = "#d8d8d8",
+    }),
+    de.substyle("*-selection", {
+        background_colour = "#f0c000",
+        foreground_colour = "#000000",
+    }),
+})
+
+gr.refresh()
+
