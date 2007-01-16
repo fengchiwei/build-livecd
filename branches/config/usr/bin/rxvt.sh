fcb3eaff54390d18dc412a15cdb0c0964082195a
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2007-01-16 00:51:14 +0000

    build-livecd branch for Edgy

---
+++ branches/config/usr/bin/rxvt.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rxvt-unicode -fn -adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1 -bg black -fg gray &
---
+++ branches/config/usr/local/bin/pud-embed-to-hd.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# pud-embed-to-hd.pl - embedding PUD GNU/Linux to Win32 partition
+
+# Copyright 2006 by 
+# Pin-Shiun Chen (penkia) <penkia@gmail.com>
+
+# This program is free software; 
+# you can redistribute it and/or modify it under the same terms as Perl itself.
+# See http://www.perl.com/perl/misc/Artistic.html
+
+use strict; 
+use warnings;
+my @disk = `fdisk -l`;
+my $target;
+foreach (@disk) {
+next unless /^\/dev\/(\w+)\s/;
+my $device = $1;
+$target = "/mnt/$device" if -e "/mnt/$device/boot.ini";
+}
+
+if ($target) {
+print "Now embedding PUD to $target...\n";
+
+my $tmp = `grep PUD $target/boot.ini`;
+if (! defined $tmp) {
+print "Modify $target/boot.ini ...";
+!system("perl -pi -e 's!timeout=30!timeout=10!' $target/boot.ini") or die "$!\n";
+!system(qq{echo 'c:\\grldr="PUD GNU/Linux"' >> $target/boot.ini}) or die "$!\n";
+print "OK.\n";
+} else {
+print "It seems you have embedded before, it will now be upgraded.";
+}
+
+print "Copy all necessary files (this may take a while) ...";
+!system("cp -rf /cdrom/opt/ /cdrom/casper/ /cdrom/initrd.gz /cdrom/vmlinuz /cdrom/misc/embedded/menu.lst /cdrom/misc/embedded/grldr $target") or die "$!\n";
+print "OK.\n";
+
+print "Done. You can reboot and try the embedded PUD GNU/Linux. \n";
+
+} else {
+print "Can not find any bootable W32 partition with 'boot.ini', abort.\n";
+}
