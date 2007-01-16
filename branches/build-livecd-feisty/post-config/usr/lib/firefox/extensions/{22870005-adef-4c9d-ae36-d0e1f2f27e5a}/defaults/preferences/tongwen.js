f79fa143820b0f1d57939cab5110e55ecb5a5337
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2007-01-16 01:20:35 +0000

    build-livecd branch for Feisty

---
+++ branches/build-livecd-feisty/post-config/usr/lib/firefox/extensions/{22870005-adef-4c9d-ae36-d0e1f2f27e5a}/defaults/preferences/tongwen.js
@@ -0,0 +1,48 @@
+pref("tongwentang.autoconvert.chineseCharset", "off");
+pref("tongwentang.autoconvert.oppositeCharsetOnly", true);
+pref("tongwentang.autoconvert.chineseEncodingOnly", true);
+pref("tongwentang.autoconvert.enableUtf8Trans", true);
+
+pref("tongwentang.debug", "off");
+pref("tongwentang.reloadSetting", false);
+pref("tongwentang.phrase.useInsideTable", true);
+pref("tongwentang.phrase.useOutsideTable", false);
+pref("tongwentang.phrase.outsideTablePath", "default");
+pref("tongwentang.tools.useExtraConvertTool", false);
+pref("tongwentang.tools.extraConvertToolPath", "");
+pref("tongwentang.tools.useAdvanceSetting", false);
+pref("tongwentang.tools.convertTrad2Simp", "/i:utf8 /o:utf8 /f:s");
+pref("tongwentang.tools.convertSimp2Trad", "/i:utf8 /o:utf8 /f:t");
+pref("tongwentang.textZoom.enable", false);
+pref("tongwentang.textZoom.percent", 100);
+pref("tongwentang.textZoom.autoDefault", false);
+pref("tongwentang.textZoom.standalone", false);
+
+pref("tongwentang.convLazy", true);
+pref("tongwentang.convMethod", "auto");
+
+pref("tongwentang.contextMenuIcon"     , true);
+pref("tongwentang.contextMenu"         , true);
+pref("tongwentang.contextMenu.Auto"    , true);
+pref("tongwentang.contextMenu.InputSim", true);
+pref("tongwentang.contextMenu.InputTra", true);
+pref("tongwentang.contextMenu.InputZhu", true);
+pref("tongwentang.contextMenu.PageSim" , true);
+pref("tongwentang.contextMenu.PageTra" , true);
+pref("tongwentang.contextMenu.PageZhu" , true);
+pref("tongwentang.contextMenu.ClipSim" , true);
+pref("tongwentang.contextMenu.ClipTra" , true);
+pref("tongwentang.contextMenu.TextZoom", true);
+pref("tongwentang.contextMenu.Setting" , true);
+
+pref("tongwentang.filter.enable" , false);
+pref("tongwentang.filter.list" , "");
+
+pref("tongwentang.redefinephrase.enable" , false);
+pref("tongwentang.redefinephrase.s2t" , "");
+pref("tongwentang.redefinephrase.t2s" , "");
+
+pref("tongwentang.redefinephrase2.s2t" , "");
+pref("tongwentang.redefinephrase2.t2s" , "");
+
+pref("tongwentang.toolbar.enable" , true);
