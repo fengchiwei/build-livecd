522961ba82222f44d25e856040b10f5f8e483670
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2007-01-16 01:06:19 +0000

    build-livecd branch for Edgy

---
+++ branches/build-livecd-edgy/post-config/usr/lib/firefox/extensions/videodowloader@videodownloader.net/defaults/preferences/videodownloader.js
@@ -0,0 +1,3 @@
+pref("extensions.videodownloader.boolpref", true);
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.videodowloader@videodownloader.net.description", "chrome://videodownloader/locale/videodownloader.properties");
