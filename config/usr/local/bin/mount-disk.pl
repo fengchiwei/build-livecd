22bb825baaa198b184b0c926fc2bf012f8f7dbc7
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2007-04-23 18:48:41 +0000

    update mount-disk.pl to swapon

--- config/usr/local/bin/mount-disk.pl
+++ config/usr/local/bin/mount-disk.pl
@@ -22,14 +22,23 @@ chomp($fstype = `/lib/udev/vol_id -t /dev/$1`);
 switch ($fstype) {
 case 'ntfs' { 	!system("ntfs-3g /dev/$device /mnt/$device -o noatime,silent,umask=0,locale=zh_TW.utf8") 
 		#!system("ntfsmount /dev/$device /mnt/$device -o show_sys_files,umask=0,locale=zh_TW.UTF8") 
-		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); } 
+		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); 
+	} 
 
 case 'vfat' { 	!system("mount /dev/$device /mnt/$device -o noatime,iocharset=utf8")
-		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); }
+		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); 
+	}
 
 case /(ext2|ext3)/ { 
 		!system("mount -o noatime /dev/$device /mnt/$device")
-		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); } 
+		or (rmdir("/mnt/$device") && unlink("/home/ubuntu/Desktop/$device.desktop")); 
+	} 
+
+case 'swap' {
+		!system("swapon /dev/$device");
+		rmdir("/mnt/$device");
+		unlink("/home/ubuntu/Desktop/$device.desktop"); 
+	}
 
 else 	{
 		!system("mount -o noatime /dev/$device /mnt/$device")
