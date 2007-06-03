51c956d0b21225efdc69bec29c14ddd6092712cf
penkia <penkia@2bd6256b-561c-0410-a88f-6db8428a4865>
2007-06-03 15:57:55 +0000

    1) add mkliveusb project
    2) update remote-opt-get
    
    3_ fix typo in /etc/init.d/startx
    
    4) fix encoding in youtube.xml
    
    5) take some locale from remove.txt
    
    6) change default terminal font

---
+++ branches/build-livecd-feisty/config/usr/local/bin/mkliveusb.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# mkliveusb.pl - PUD GNU/Linux LiveUSB Installer
+# Copyright 2007 by Pin-Shiun Chen (penkia) <penkia@gmail.com>.
+# This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+# See http://www.perl.com/perl/misc/Artistic.html
+
+use mkliveusb;
+
+Gtk2->main;
+
